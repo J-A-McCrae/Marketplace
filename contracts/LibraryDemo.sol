8051d5d1d9f64b4b7b5f31983743ebcadaecc03a
J-A-McCrae <50707047+J-A-McCrae@users.noreply.github.com>
2019-07-31 20:25:44 +0100

    Revised LibraryDemo.sol

--- contracts/LibraryDemo.sol
+++ contracts/LibraryDemo.sol
@@ -1,49 +1,106 @@
-/// @title EmergencyStop: A circuit breaker for temporarily disabling contract functions
-/// @author John McCrae
 
-/// @dev This contract requires solidity compiler version 0.5 or higher
+/// @title LibraryDemo: A demo contract showcasing the use of libraries in solidity
+/// @author John McCrae
+/// @notice This contract may be evaluated at https://remix.ethereum.org
+
+/// @dev This contract requires solidity compiler version 0.5 or higher.
 pragma solidity ^0.5.0;
 
-/// @dev EmergencyStop: A contract that implements the circuit breaker design pattern
-contract EmergencyStop {
-    
-    /// @param ownerESC A public payable address data type
-    address payable public ownerESC;
-    
-    /// @param emergencyStopSwitch A public boolean data type to keep track of emergencyStopSwitch (circuit breaker flag)
-    bool public emergencyStopSwitch;
-   
-    /// @param _emergencySwitchValue A boolean data type to keep track of changes to emergencyStopSwitch
-    event LogEmergencyStopSwitch(bool _emergencySwitchValue);
-   
-    /// @dev verifyOwnerESC A modifier requiring the message sender address is equal to the owner address
-    modifier verifyOwnerESC () {
-        require(ownerESC == msg.sender, "Access denied. Access restricted to contract owner.");
-        _;
-    }
-   
-    /// @dev verifyEmergencyStopValue A modifier requiring emergencyStopSwitch (circuit breaker flag) to be false
-    modifier verifyEmergencyStopValue () {
-        require(emergencyStopSwitch == false, "Access denied. This contract has been temporarily disabled.");
-        _;
-    }
+/// @dev **Please delete the four lines indicated prior to evlaution. // **DELETE THIS LINE**
+contract DeleteMe {constructor() public {}} // **DELETE THIS LINE**
 
-    /// @dev Declare constructor. Set ownerESC to be the contract creator.
-    constructor () public {
-        ownerESC = msg.sender;
-    }
+/* // **DELETE THIS LINE**
+/// @dev This contract utilizes ABIEncoderV2 (Supported in Remix)
+pragma experimental ABIEncoderV2;
 
-    /// @dev emergencyStopFunction() A function that allows setting emergencyStopSwitch (circuit breaker flag) to true or false
-    /// @param emergencySwitchValue A boolean data type for changing the state of emergencyStopSwitch (circuit breaker flag)
-    /// @return A boolean data type returning the new value of emergencyStopSwitch (circuit breaker flag)
-    function emergencyStopFunction (bool emergencySwitchValue)
-    public
-    verifyOwnerESC
-    returns(bool _emergencyStopSwitch)
+/// @dev Demo: A library for calculating the total stock value of a product in the store
+library Demo {
+    
+    /// @param productName A string data type to keep track of store product names
+    /// @param totalUnits A uint data type to keep track of total store product units
+    /// @param unitPrice A uint data type to keep track of store product unit prices
+    /// @param forSale A uint data type to keep track of whether store product is for sale
+    /// @param buyersAcc A uint data type to keep track of total product units held within the buyers account
+    struct Products
     {
-        emit LogEmergencyStopSwitch(emergencySwitchValue);
-        emergencyStopSwitch = emergencySwitchValue;
-        return emergencyStopSwitch;
+        string productName;
+        uint totalUnits;
+        uint unitPrice;
+        bool forSale;
+        uint buyersAcc;
     }
-    
+
+    /// @dev calcStockValueUsingLib() A function to calculate and return the stock value of a product
+    /// @param _storeProd A Products data type from the calling contract containing the struct product data
+    /// @return _stockProduct, stockValue
+    function _calcStockValueUsingLib (Products memory _storeProd)
+        public
+        pure
+        returns (string memory _stockProduct, uint _stockValue)
+    {
+        string memory stockProduct = _storeProd.productName;
+        uint stockValue = _storeProd.totalUnits * _storeProd.unitPrice;
+        return (stockProduct, stockValue);
+    }
+
 }
+
+/// @dev LibraryDemo: A contract for interacting with the Demo library
+contract LibraryDemo {
+
+    /// @param storeProd A Demo.Products data type to keep track of store product data
+    Demo.Products storeProd;
+
+    /// @dev Declaration to use the Demo library for the Demo.Products data type
+    using Demo for Demo.Products;
+
+    /// @dev clearProducts() A function to allowing clearing of data in the storeProd struct
+    /// @return true
+    function clearProducts ()
+        public
+        returns (bool)
+    {
+        storeProd.productName = "";
+        storeProd.totalUnits = 0;
+        storeProd.unitPrice = 0;
+        storeProd.forSale = false;
+        storeProd.buyersAcc = 0;
+        return true;
+    }
+
+    /// @dev writeProducts() A function to allow inputting new data into the storeProd struct
+    /// @return true
+    function writeToProducts (string memory _productName, uint _totalUnits, uint _unitPrice, bool _forSale, uint _buyersAcc)
+        public
+        returns (bool)
+    {
+        storeProd.productName = _productName;
+        storeProd.totalUnits = _totalUnits;
+        storeProd.unitPrice = _unitPrice;
+        storeProd.forSale = _forSale;
+        storeProd.buyersAcc = _buyersAcc;
+        return true;
+    }
+
+    /// @dev readProducts() A function to allow reading of data in the storeProd struct
+    /// @return storeProd.productName, storeProd.totalUnits, storeProd.unitPrice, storeProd.forSale, storeProd.buyersAcc
+    function readProducts ()
+        public
+        view
+        returns (string memory _productName, uint _totalUnits, uint _unitPrice, bool _forSale, uint _buyersAcc)
+    {
+        return(storeProd.productName, storeProd.totalUnits, storeProd.unitPrice, storeProd.forSale, storeProd.buyersAcc);
+    }
+
+    /// @dev calcStockValueUsingLibrary() A function that calls the assocaited library function to get the stock value
+    /// @return storeProd._calcStockValueUsingLib()
+    function calcStockValueUsingLibrary ()
+        public
+        view
+        returns (string memory _stockProduct, uint _stockValue)
+    {
+        return storeProd._calcStockValueUsingLib();
+    }
+
+}
+*/ // **DELETE THIS LINE**
